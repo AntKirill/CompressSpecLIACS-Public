33bf5cbd423d3b81786fbe455e5682705a703893
Kirill Antonov <kirant9797@gmail.com>
2023-02-01 22:19:51 +0100

    refactor and add watching of properties of multiple objects

--- mylogger.py
+++ mylogger.py
@@ -1,8 +1,8 @@
 import os
 import json
-import numpy as np
+from typing import List
 
-import mylogger
+import numpy as np
 
 
 class LoggingBestSoFar:
@@ -36,6 +36,13 @@ class MyLogger:
                      'scenarios': []}
         self.myLogStrategy = logStrategy()
         self.isLogArg = isLogArg
+        self.instance = None
+        self.meta_full_path = None
+        self.meta_path = None
+        self.log_file_full_path = None
+        self.log_file_path = None
+        self.all_extra_info_getters = []
+        self.algorithms = []
 
     @staticmethod
     def __generate_dir_name(name, x=0):
@@ -47,28 +54,30 @@ class MyLogger:
             else:
                 x = x + 1
 
-    def watch(self, algorithm, extra_data):
-        self.algorithm = algorithm
-        self.extra_info_getters = extra_data
+    def watch(self, algorithm, extra_data: List):
+        self.algorithms.append(algorithm)
+        self.all_extra_info_getters.append(extra_data)
 
-    def set_up_logger_for_problem(self, function_id, function_name, isMaximization, instance, dim):
+    def set_up_logger_for_problem(self, function_id, function_name, is_maximization, instance, dim):
         self.log_file_path = f'data_f{function_id}_{function_name}/IOHprofiler_f{function_id}_DIM{dim}.dat'
         self.log_file_full_path = f'{self.folder_name}/{self.log_file_path}'
         self.meta_path = f'IOHprofiler_f{function_id}_{function_name}.json'
         self.meta_full_path = f'{self.folder_name}/{self.meta_path}'
         self.meta['function_id'] = function_id
         self.meta['function_name'] = function_name
-        self.meta['maximization'] = isMaximization
+        self.meta['maximization'] = is_maximization
         self.instance = instance
         self.meta['scenarios'].append({'dimension': dim, 'path': self.log_file_path, 'runs': [
                                       {'instance': instance, 'evals': 0, 'best': {}}]})
         os.makedirs(os.path.dirname(self.log_file_full_path), exist_ok=True)
-        for extra_info in self.extra_info_getters:
-            self.meta['attributes'].append(extra_info)
+        for extra_info_getter in self.all_extra_info_getters:
+            for extra_info in extra_info_getter:
+                self.meta['attributes'].append(extra_info)
         with open(self.log_file_full_path, 'a') as f:
             f.write('evaluations raw_y')
-            for extra_info in self.extra_info_getters:
-                f.write(f' {extra_info}')
+            for extra_info_getter in self.all_extra_info_getters:
+                for extra_info in extra_info_getter:
+                    f.write(f' {extra_info}')
             if self.isLogArg:
                 for i in range(dim):
                     f.write(f' x{i}')
@@ -77,11 +86,12 @@ class MyLogger:
     def log_eval(self, evaluation_number, arg, value):
         with open(self.log_file_full_path, 'a') as f:
             f.write(f'{evaluation_number} {value}')
-            for fu in self.extra_info_getters:
-                try:
-                    extra_info = getattr(self.algorithm, fu)
-                except Exception as e:
-                    extra_info = 'None'
+            for i in range(len(self.all_extra_info_getters)):
+                for fu in self.all_extra_info_getters[i]:
+                    try:
+                        extra_info = getattr(self.algorithms[i], fu)
+                    except Exception:
+                        extra_info = 'None'
                 f.write(f' {extra_info}')
             if self.isLogArg:
                 for element in arg:
@@ -96,7 +106,7 @@ class MyLogger:
                 return float(obj)
             if isinstance(obj, np.ndarray):
                 return obj.tolist()
-            return super(mylogger.MyLogger.NpEncoder, self).default(obj)
+            return super(MyLogger.NpEncoder, self).default(obj)
 
     def log_meta(self, evaluation_number, arg, value, eval_min, arg_min, min_dist):
         self.meta['scenarios'][0]['runs'][0] = {
