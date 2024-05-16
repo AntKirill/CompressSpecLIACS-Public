b4cc8264c0de934172922094c48d6196ca16af0f
Kirill Antonov <kirant9797@gmail.com>
2024-05-16 14:29:58 +0200

    update

--- resampleArchive.py
+++ resampleArchive.py
@@ -6,25 +6,26 @@ import utils
 import utilsV3
 
 
-def process_row_k(F, filepath, k):
+def process_row_k(F, filepath, kFrom, kTo):
     experiment_root = os.path.dirname(filepath)
     archive_resampling_dir = os.path.join(experiment_root, f'archiveResampling')
     os.makedirs(archive_resampling_dir, exist_ok=True)
-    outputpath = os.path.join(archive_resampling_dir, f'samples_for_solutions_from_{k}_to_{k}.csv')
+    outputpath = os.path.join(archive_resampling_dir, f'samples_for_solutions_from_{kFrom}_to_{kTo}.csv')
     df = pd.read_csv(filepath, sep=' ', header=None)
-    row = df.iloc[k].tolist()
-    obj_value, design = row[0], [int(i) for i in row[1:]]
-    obj_value1 = F(design, 10000)
-    samples = F.get_measurements()
-    print('Old', obj_value, 'New', obj_value1)
     with open(outputpath, 'w') as file:
-        print(*design, *samples, file=file)
+        for k in range(kFrom, kTo + 1):
+            row = df.iloc[k].tolist()
+            obj_value, design = row[0], [int(i) for i in row[1:]]
+            obj_value1 = F(design, 10000)
+            samples = F.get_measurements()
+            print('Old', obj_value, 'New', obj_value1)
+            print(*design, *samples, file=file)
 
 
-def main(filepath, k):
+def main(filepath, kFrom, kToIncl):
     D = utilsV3.CriteriaD()
     F = utilsV3.CriteriaF(D)
-    process_row_k(F, filepath, int(k))
+    process_row_k(F, filepath, int(kFrom), int(kToIncl))
 
 
 if __name__ == '__main__':
